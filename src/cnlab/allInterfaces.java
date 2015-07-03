b42d7286f27d3967e9e97fb4722d31e619fa832e
pradeepb6 <pradeepbaradur6@gmail.com>
2015-07-04 01:21:30 +0200

    working code

--- src/cnlab/allInterfaces.java
+++ src/cnlab/allInterfaces.java
@@ -5,7 +5,7 @@ package cnlab;
  */
 public interface allInterfaces {
     public String convertIPIntDec2StringBinary(int intIP);
-    public void StrIP2Dec(String input);
+    public boolean StrIP2Dec(String input);
     public int convertStrIP2IntIP(String ip) throws Exception;
     public String convertIntIP2StrIP(int in);
 
